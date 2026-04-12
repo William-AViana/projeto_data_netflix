0588b4f53cc4063bdcc17a531472e77145e01b49
William-AViana <willdev.viana@gmail.com>
2026-04-12 16:11:42 -0300

    update file with functions  load_data and validate_datas

---
+++ app/processor.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import zipfile
+import os
+
+def load_data() -> pd.DataFrame | None:
+    all_data = []
+    directory = './data'
+    
+    for item in os.listdir(directory):
+        if item.endswith('.zip'):
+            path_zip = os.path.join(directory, item)
+            with zipfile.ZipFile(path_zip, 'r') as unzip_file:
+                for file in unzip_file.namelist():
+                    if file.endswith('.csv'):
+                        with unzip_file.open(file) as f:
+                            df = pd.read_csv(f)
+                            all_data.append(df)
+                        
+    if all_data:
+        all_data_frame = pd.concat(all_data, ignore_index=True)
+        return all_data_frame
+    return None
+    
+def validade_datas(df=load_data()):
+    print(type(df))
+    if df is not None:
+        total_titles = len(df)
+        years = df['release_year'].unique()
+        return {
+                "total_titles": total_titles,
+                "period": f"{np.min(years)} - {np.max(years)}"
+            }
+    return {"error": "Datas is not find"}
